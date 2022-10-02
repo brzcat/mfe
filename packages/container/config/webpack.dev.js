d30d881fe57856dd83a29641188d776f7c728178
kelvin <kellai@gmail.com>
2022-10-02 16:54:13 -0700

    initalize container with react

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,24 @@
+// merge allows us to take all the config inside common file and merge it
+// with configuration inside development file.
+const {merge} = require('webpack-merge');
+// takes some kind of HTML file inside of project and inject a couple of different scrip ttags inside of it
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// to take our dev config and merge together with config inside common file we want to merge it later using merge.
+const commonConfig = require('./webpack.common');
+
+const devConfig = {
+    mode: 'development',
+    devServer: {
+        port: 8080,
+        historyApiFallback: {
+            index: 'index.html'
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ],
+};
+// listing dev config second, means that the dev config is going ot override similar config assigned to common config.
+module.exports = merge(commonConfig, devConfig);
