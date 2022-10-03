0eea390639daba937836f7e95aa15dde4b7449f5
kelvin <kellai@gmail.com>
2022-10-02 17:43:39 -0700

    add shared modules

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -21,7 +21,8 @@ const devConfig = {
            name: 'container',
            remotes: {
                marketing: 'marketing@http://localhost:8081/remoteEntry.js'
-           }
+           },
+            shared: ['react', 'react-dom']
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html'
