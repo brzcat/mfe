10b4bfe6f49e55c3fc77f1f040d75a7a7fe6cd17
kelvin <kellai@gmail.com>
2022-10-16 21:02:25 -0700

    add history API fallback

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -15,9 +15,7 @@ const devConfig = {
     mode: 'development',
     devServer: {
         port: 8081,
-        historyApiFallback: {
-            index: 'index.html'
-        }
+        historyApiFallback: true
     },
     plugins: [
         new ModuleFederationPlugin({
