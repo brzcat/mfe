dcbcfe1a36be97efb428be8d112edac1104229c6
kelvin <kellai@gmail.com>
2022-10-30 15:29:43 -0700

    integrate dashboard in vue with container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -10,7 +10,7 @@ import Progress from './components/Progress'
 import AuthApp from "./components/AuthApp";
 const MarketingLazy = lazy (() => import('./components/MarketingApp'));
 const AuthLazy = lazy (() => import('./components/AuthApp'));
-
+const DashboardLazy = lazy(() => import('./components/DashboardApp'));
 
 
 const generateClassName = createGenerateClassName({
@@ -31,6 +31,7 @@ export default () => {
               <Route path="/auth">
                 <AuthLazy onSignIn={() => setIsSignedIn(true)}/>
               </Route>
+              <Route path="/dashboard" component={DashboardLazy}/>
               <Route path="/" component={MarketingLazy} />
             </Switch>
           </Suspense>
